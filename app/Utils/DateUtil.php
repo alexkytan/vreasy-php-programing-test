9d1154ef6a9f317b7bed9f3d2c9db542298b5f6e
alexk-ddi <oleksandr.kytan.ddi@gmail.com>
2019-05-28 09:55:08 +0300

    Add DateUtil

---
+++ app/Utils/DateUtil.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class DateUtil
+{
+    const DEFAULT_TIMEZONE = 'UTC';
+    const DEFAULT_DATEFORMAT = \DateTime::ISO8601;
+
+    /**
+     * @param string $value
+     * @param string $timezone
+     * @param string $dateFormat
+     * @return string
+     * @throws \Exception
+     */
+    public static function getDateWithTimezone(
+        string $value,
+        string $timezone = self::DEFAULT_TIMEZONE,
+        string $dateFormat = self::DEFAULT_DATEFORMAT
+    ): string
+    {
+        $date = new \DateTime($value);
+        $date->setTimezone(new \DateTimeZone($timezone));
+
+        return $date->format($dateFormat);
+    }
+}
